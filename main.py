67d357947b3219e761bdc2c5b760ef0c23443798
cashbooda <kjprogrammerpc@gmail.com>
2024-06-23 11:23:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-
-
 from typing import Final
 import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 
-# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-# STEP 1: BOT SETUP
 intents: Intents = Intents.default()
 intents.message_content = True  # NOQA
 client: Client = Client(intents=intents)
 
-
-# STEP 2: MESSAGE FUNCTIONALITY
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
         print('(Message was empty because intents were not enabled probably)')
@@ -31,14 +25,11 @@ async def send_message(message: Message, user_message: str) -> None:
     except Exception as e:
         print(e)
 
-
-# STEP 3: HANDLING THE STARTUP FOR OUR BOT
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
 
 
-# STEP 4: HANDLING INCOMING MESSAGES
 @client.event
 async def on_message(message: Message) -> None:
     if message.author == client.user:
@@ -51,15 +42,11 @@ async def on_message(message: Message) -> None:
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
 
-
-# STEP 5: MAIN ENTRY POINT
 def main() -> None:
     client.run(token=TOKEN)
 
 
 if __name__ == '__main__':
     main()
-
-
 
 
